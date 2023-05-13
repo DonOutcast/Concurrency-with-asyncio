c5e06d052d3b459d41acd0a08c3d219284602c5a
DonOutcast <lymondgl@student.21-school.ru>
2023-05-13 10:06:27 +0300

    lisitin_2_4

---
+++ src/chapter_02/listing_2_4.py
@@ -0,0 +1,13 @@
+import asyncio
+
+async def add_one(number: int) -> int:
+    return number + 1
+
+async def main() -> None:
+    one_plus_one = await add_one(1)
+    two_plus_two = await add_one(2)
+    print(one_plus_one)
+    print(two_plus_two)
+
+asyncio.run(main())
+
