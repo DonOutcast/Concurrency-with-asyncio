bd3b94ede81128f7d1ee6d41cf4ca043510c55cb
DonOutcast <lymondgl@student.21-school.ru>
2023-05-13 10:17:44 +0300

    listing_2_5

---
+++ src/chapter_02/listing_2_5.py
@@ -0,0 +1,12 @@
+import asyncio
+
+async def hello_world_message() -> str:
+    await asyncio.sleep(1)
+    return 'Hello, World!'
+
+async def main() -> None:
+    message = await hello_world_message()
+    print(message)
+
+asyncio.run(main())
+
