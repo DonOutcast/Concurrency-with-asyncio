8991659b25facfaec0faee31aab6fc0b6d9617cb
DonOutcast <sham1996@yandex.ru>
2023-05-15 07:52:59 +0300

    listing_2_9

---
+++ src/chapter_02/listing_2_9.py
@@ -0,0 +1,15 @@
+import asyncio
+from src.util.delay_functions import delay
+
+
+async def main() -> None:
+    sleep_for_three = asyncio.create_task(delay(3))
+    sleep_again = asyncio.create_task(delay(3))
+    sleep_once_more = asyncio.create_task(delay(3))
+
+    await sleep_for_three
+    await sleep_again
+    await sleep_once_more
+
+
+asyncio.run(main())
