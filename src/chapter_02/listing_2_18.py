e68457b34dddc057a23d719bd211840832502543
DonOutcast <sham1996@yandex.ru>
2023-05-16 08:51:06 +0300

    listing_2_19

---
+++ src/chapter_02/listing_2_18.py
@@ -0,0 +1,21 @@
+import asyncio
+from src.util.asyn_timer import async_timed
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(1_000_000):
+        counter += 1
+    return counter
+
+
+@async_timed()
+async def main() -> None:
+    task_one = asyncio.create_task(cpu_bound_work())
+    task_two = asyncio.create_task(cpu_bound_work())
+    await task_one
+    await task_two
+
+
+asyncio.run(main())
