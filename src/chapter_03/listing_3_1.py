5668c0911505d5d9823833a9bc9420e87db563ab
DonOutcast <sham1996@yandex.ru>
2023-05-18 08:17:37 +0300

    listing_3_1

---
+++ src/chapter_03/listing_3_1.py
@@ -0,0 +1,10 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+connection, client_address = server_socket.accept()
+print(f"Получен запрос на подключение от {client_address}!")
