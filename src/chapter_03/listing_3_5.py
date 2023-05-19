4511904cb8902f455b8d3dd7da43ca8c8596ee4a
DonOutcast <sham1996@yandex.ru>
2023-05-19 08:11:40 +0300

    listing_3_5

---
+++ src/chapter_03/listing_3_5.py
@@ -0,0 +1,33 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ("127.0.0.1", 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+server_socket.setblocking(False)
+
+connections = []
+try:
+    while True:
+        connection, client_address = server_socket.accept()
+        connection.setblocking(False)
+        print(f"Получен запрос на подключение от {client_address}!")
+        connections.append(connection)
+
+        for connection in connections:
+            buffer = b''
+
+            while buffer[-2:] != b'\r\n':
+                data = connection.recv(2)
+                if not data:
+                    break
+                else:
+                    print(f"Полученные данные: {data}!")
+                    buffer += data
+
+                print(f"Все данные: {buffer}")
+                connection.send(buffer)
+finally:
+    server_socket.close()
