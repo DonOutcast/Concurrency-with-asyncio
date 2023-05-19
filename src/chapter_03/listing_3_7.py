6056ff65ccf7feb1dc58b05a64bb0a9abbfd0ff5
DonOutcast <sham1996@yandex.ru>
2023-05-19 08:32:50 +0300

    listing_3_7

---
+++ src/chapter_03/listing_3_7.py
@@ -0,0 +1,36 @@
+import socket
+import selectors
+from selectors import SelectorKey
+from typing import List, Tuple
+
+selector = selectors.DefaultSelector()
+
+server_socket = socket.socket()
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ("127.0.0.1", 8000)
+server_socket.setblocking(False)
+server_socket.bind(server_address)
+server_socket.listen()
+
+selector.register(server_socket, selectors.EVENT_READ)
+
+while True:
+    events: List[Tuple[SelectorKey, int]] = selector.select(timeout=1)
+
+    if len(events) == 0:
+        print("Событий нет, подожду еще!")
+
+    for event, _ in events:
+        event_socket = event.fileobj
+
+        if event_socket == server_socket:
+            connection, address = server_socket.accept()
+            connection.setblocking(False)
+            print(f"Получен запрос на подключение от {address}")
+            selector.register(connection, selectors.EVENT_READ)
+        else:
+            data = event_socket.recv(1024)
+            print(f"Получены данные: {data}")
+            event_socket.send(data)
+
