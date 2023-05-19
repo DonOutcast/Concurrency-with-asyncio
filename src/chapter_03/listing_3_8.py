3724226291560780feef4190ac27d3f2e282c41c
DonOutcast <sham1996@yandex.ru>
2023-05-19 08:48:53 +0300

    listing_3_8

---
+++ src/chapter_03/listing_3_8.py
@@ -0,0 +1,30 @@
+import socket
+import asyncio
+from asyncio import AbstractEventLoop
+
+async def echo(connection: socket, loop: AbstractEventLoop) -> None:
+    while data := await loop.sock_recv(connection, 1024):
+        await loop.sock_sendall(connection, data)
+
+
+async def listen_for_connection(server_socket: socket, loop: AbstractEventLoop) -> None:
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f"Получен запрос на подключение от {address}")
+        asyncio.create_task(echo(connection, loop))
+
+
+async def main() -> None:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    await listen_for_connection(server_socket, asyncio.get_event_loop())
+
+
+asyncio.run(main())
