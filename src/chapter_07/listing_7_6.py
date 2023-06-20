b6c628bbd152bf12e291ae5635c2bf06dd699467
DonOutcast <sham1996@yandex.ru>
2023-06-20 07:27:57 +0300

    listing_7_6

---
+++ src/chapter_07/listing_7_6.py
@@ -0,0 +1,23 @@
+import functools
+import requests
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from src.util.asyn_timer import async_timed
+
+
+def get_status_code(url: str) -> int:
+    response = requests.get(url)
+    return response.status_code
+
+
+@async_timed()
+async def main():
+    loop = asyncio.get_running_loop()
+    urls = ["https://www.example.com" for _ in range(1000)]
+    tasks = [loop.run_in_executor(None, functools.partial(get_status_code, url)) for url in urls]
+    results = await asyncio.gather(*tasks)
+    print(results)
+
+
+asyncio.run(main())
+
