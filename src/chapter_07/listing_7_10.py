2308251e3c1ad5ef552a94264c89eb12b560eaef
DonOutcast <sham1996@yandex.ru>
2023-06-21 07:35:11 +0300

    listing_7_10

---
+++ src/chapter_07/listing_7_10.py
@@ -0,0 +1,24 @@
+from typing import List
+from threading import RLock
+
+
+class InitListThreadsafe:
+
+    def __init__(self, wrapped_list: List[int]):
+        self._lock = RLock()
+        self._inner_list = wrapped_list
+
+    def indices_of(self, to_find: int) -> List[int]:
+        with self._lock:
+            enumerator = enumerate(self._inner_list)
+            return [index for index, value in enumerator if value == to_find]
+
+    def find_and_replace(self, to_replace: int, replace_with: int) -> None:
+        with self._lock:
+            indices = self.indices_of(to_replace)
+            for index in indices:
+                self._inner_list[index] = replace_with
+
+
+threadsafe_list = InitListThreadsafe([1, 2, 1, 2, 1])
+threadsafe_list.find_and_replace(1, 2)
