fb2860d17bda0f78986064a9193b4d5966ad7f1e
DonOutcast <sham1996@yandex.ru>
2023-06-24 07:38:08 +0300

    deleted file lesson.py

--- lesson.py
+++
@@ -1,9 +0,0 @@
-
-def is_palindrome(string):
-    return string.strip() == string[::-1].strip()
-
-
-if __name__ == "__main__":
-    user_word = input()
-    result = is_palindrome(user_word)
-    print(result)
