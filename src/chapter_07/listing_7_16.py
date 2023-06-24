bd9c86425decc06f7c9a2da72e2862ae589f82a2
DonOutcast <sham1996@yandex.ru>
2023-06-24 08:17:35 +0300

    listing_7_16

---
+++ src/chapter_07/listing_7_16.py
@@ -0,0 +1,26 @@
+import os
+import time
+import string
+import random
+import hashlib
+
+
+def random_password(length: int) -> bytes:
+    ascii_lowercase = string.ascii_lowercase.encode()
+    return b''.join(bytes(random.choice(ascii_lowercase)) for _ in range(length))
+
+
+passwords = [random_password(10) for _ in range(10000)]
+
+
+def hash(password: bytes) -> str:
+    salt = os.urandom(16)
+    return str(hashlib.scrypt(password, salt=salt, n=2048, p=1, r=8))
+
+
+start = time.time()
+for password in passwords:
+    hash(password)
+
+end = time.time()
+print(end - start)
