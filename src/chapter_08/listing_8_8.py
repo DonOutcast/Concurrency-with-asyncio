824072dd761eef506ddc1b7ccac6b7fd8a9fd866
DonOutcast <sham1996@yandex.ru>
2023-06-25 09:27:59 +0300

    listing_8_8

---
+++ src/chapter_08/listing_8_8.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+from asyncio import StreamReader
+from listing_8_7 import move_back_one_char, clear_line
+
+
+async def read_line(stdin_reader: StreamReader) -> str:
+    def erase_last_char():
+        move_back_one_char()
+        sys.stdout.write(' ')
+        move_back_one_char()
+
+    delete_char = b'\x7f'
+    input_buffer = deque()
+    while (input_char := await stdin_reader.read(1)) != b'\n':
+        if input_char == delete_char:
+            if len(input_buffer) > 0:
+                input_buffer.pop()
+                erase_last_char()
+                sys.stdout.flush()
+            else:
+                input_buffer.append(input_char)
+                sys.stdout.write(input_char.decode())
+                sys.stdout.flush()
+    clear_line()
+    return b''.join(input_buffer).decode()
