bf77fd7bd66074f63984a6e59c58d4ffdb7534b1
DonOutcast <sham1996@yandex.ru>
2023-06-30 09:00:18 +0300

    listing_10_6

--- src/chapter_10/listing_10_6.py
+++ src/chapter_10/listing_10_6.py
@@ -6,16 +6,16 @@ from listing_10_4 import DB_KEY, create_database_pool, destroy_database_pool
 
 routes = web.RouteTableDef()
 
-@routes.geet("/users/{id}/favorites")
-async def favorites(request: Request) -> Response:
+@routes.get("/users/{id}/cart")
+async def time(request: Request) -> Response:
     try:
         str_id = request.match_info["id"]
         user_id = int(str_id)
-        db = request.app[DB_KEY]
-        favorites_query = """SELECT product_id FROM user_favorite WHERE user_id = $1"""
-        result = await db.fetch(favorites_query, user_id)
+        db = request.app["DB_KEY"]
+        cart_query = """SELECT product_id FROM user_cart WHERE user_id = $1"""
+        result = await db.fetch(cart_query, user_id)
         if result is not None:
-            return web.json_response([dict(record)] for record in result)
+            return web.json_response([dict(record) for record in result])
         else:
             raise web.HTTPNotFound()
     except ValueError:
@@ -29,9 +29,11 @@ app.on_startup.append(
         port=5432,
         user="lymondgl",
         password="",
-        database="favorites"
+        database="cart",
     )
 )
 
 app.on_cleanup.append(destroy_database_pool)
-web.run_app(app, port=8002)
+
+app.add_routes(routes)
+web.run_app(app, port=8003)
