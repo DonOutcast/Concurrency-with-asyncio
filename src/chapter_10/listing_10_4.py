585486cc47a5edf1a13cb74a7d898cf3f7ac00e4
DonOutcast <sham1996@yandex.ru>
2023-06-30 08:43:22 +0300

    listing_10_4

---
+++ src/chapter_10/listing_10_4.py
@@ -0,0 +1,30 @@
+import asyncpg
+from asyncpg.pool import Pool
+from aiohttp.web_app import Application
+
+DB_KEY = "database"
+
+
+async def create_database_pool(
+        app: Application,
+        host: str,
+        port: int,
+        user: str,
+        database: str,
+        password: str
+):
+    pool: Pool = await asyncpg.create_pool(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database,
+        min_size=6,
+        max_size=6
+    )
+    app[DB_KEY] = pool
+
+
+async def destroy_database_pool(app: Application):
+    pool: Pool = app[DB_KEY]
+    await pool.close()
