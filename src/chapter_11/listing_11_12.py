c8457f7a71d89c3d6e3d555b410221c14aa00f13
DonOutcast <sham1996@yandex.ru>
2023-07-01 13:46:38 +0300

    listing_11_12

---
+++ src/chapter_11/listing_11_12.py
@@ -0,0 +1,35 @@
+import asyncio
+from asyncio import StreamWriter, StreamReader
+
+from listing_11_11 import FileUpload
+
+
+class FileServer:
+
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.upload_event = asyncio.Event()
+
+    async def start_server(self):
+        server = await asyncio.start_server(self._client_connected, self.host, self.port)
+
+        await server.serve_forever()
+
+    async def dump_contents_on_complete(self, upload: FileUpload):
+        file_contents = await upload.get_contents()
+        print(file_contents)
+
+    def _client_connected(self, reader: StreamReader, writer: StreamWriter):
+        upload = FileUpload(reader, writer)
+        upload.listen_for_uploads()
+        asyncio.create_task(self.dump_contents_on_complete(upload))
+
+
+async def main():
+    server = FileServer("127.0.0.1", 9000)
+    await server.start_server()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
