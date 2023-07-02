6e074a673bd7506267f492175d61d340f5ce0e14
DonOutcast <sham1996@yandex.ru>
2023-07-02 12:47:24 +0300

    listing_12_2

--- src/chapter_12/listing_12_2.py
+++ src/chapter_12/listing_12_2.py
@@ -1,7 +1,6 @@
 import asyncio
 from asyncio import Queue
 from random import randrange
-from typing import List
 
 
 class Product:
@@ -11,14 +10,14 @@ class Product:
 
 
 class Customer:
-    def __init__(self, customer_id: int, products: List[Product]):
+    def __init__(self, customer_id, products):
         self.customer_id = customer_id
         self.products = products
 
 
 async def checkout_customer(queue: Queue, cashier_number: int):
-    while not queue.empty():
-        customer: Customer = queue.get_nowait()
+    while True:
+        customer: Customer = await queue.get()
         print(f'Cashier {cashier_number} '
               f'checking out customer '
               f'{customer.customer_id}')
@@ -33,22 +32,39 @@ async def checkout_customer(queue: Queue, cashier_number: int):
         queue.task_done()
 
 
-async def main():
-    customer_queue = Queue()
-
+def generate_customer(customer_id: int) -> Customer:
     all_products = [Product('beer', 2),
                     Product('bananas', .5),
                     Product('sausage', .2),
                     Product('diapers', .2)]
+    products = [all_products[randrange(len(all_products))]
+                for _ in range(randrange(10))]
+    return Customer(customer_id, products)
 
-    for i in range(10):
-        products = [all_products[randrange(len(all_products))]
-                    for _ in range(randrange(10))]
-        customer_queue.put_nowait(Customer(i, products))
+
+async def customer_generator(queue: Queue):
+    customer_count = 0
+
+    while True:
+        customers = [generate_customer(i)
+                     for i in range(customer_count, customer_count + randrange(5))]
+        for customer in customers:
+            print('Waiting to put customer in line...')
+            await queue.put(customer)
+            print('Customer put in line!')
+        customer_count = customer_count + len(customers)
+        await asyncio.sleep(1)
+
+
+async def main():
+    customer_queue = Queue(5)
+
+    customer_producer = asyncio.create_task(customer_generator(customer_queue))
 
     cashiers = [asyncio.create_task(checkout_customer(customer_queue, i))
                 for i in range(3)]
 
-    await asyncio.gather(customer_queue.join(), *cashiers)
+    await asyncio.gather(customer_producer, *cashiers)
+
 
 asyncio.run(main())
