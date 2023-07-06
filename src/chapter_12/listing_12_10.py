de6cc04cabc00d2625527986539aea0f09998465
DonOutcast <sham1996@yandex.ru>
2023-07-06 09:17:26 +0300

    listign_12_10

---
+++ src/chapter_12/listing_12_10.py
@@ -0,0 +1,41 @@
+import asyncio
+from asyncio import Queue, LifoQueue
+from dataclasses import dataclass, field
+
+
+@dataclass(order=True)
+class WorkItem:
+    priority: int
+    order: int
+    data: str = field(compare=False)
+
+
+async def worker(queue: Queue):
+    while not queue.empty():
+        work_item: WorkItem = await queue.get()
+        print(f"Обрабатыветя элемент {work_item}")
+        queue.task_done()
+
+
+async def main():
+    lifo_queue = LifoQueue()
+
+    work_items = [
+        WorkItem(3, 1, "Lowest priority first"),
+        WorkItem(3, 2, "Lowest priority second"),
+        WorkItem(3, 3, "Lowest priority third"),
+        WorkItem(2, 4, "Medium priority"),
+        WorkItem(1, 5, "High priority")
+    ]
+
+    worker_task = asyncio.create_task(worker(lifo_queue))
+
+    for work in work_items:
+        lifo_queue.put_nowait(work)
+
+    await asyncio.gather(lifo_queue.join(), worker_task)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
